cfe71d39ac52633bc33c8bac356b0ec38761216a
Pororo <375369271@qq.com>
2018-07-19 15:47:30 +0800

    环境已搭，随时起飞

---
+++ src/main/java/com/douban/eggshell/EggshellApplication.java
@@ -0,0 +1,12 @@
+package com.douban.eggshell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggshellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EggshellApplication.class, args);
+	}
+}
---
+++ src/test/java/com/douban/eggshell/EggshellApplicationTests.java
@@ -0,0 +1,16 @@
+package com.douban.eggshell;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EggshellApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
